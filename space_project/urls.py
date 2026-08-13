7c771de2a87e7d6715947dfce0b1023cfccd78a3
Elijah-Ajadi <ajadiademola926@gmail.com>
2026-08-13 17:43:50 +0100

    feat: initialize project URL configuration with admin and core routes

--- space_project/urls.py
+++ space_project/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("192.28.119#.100/", admin.site.urls),
     path("",       include("core.urls")),
 ]
 
